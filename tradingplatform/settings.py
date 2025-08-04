64743d2e9d99e9219e6eaaf0f414aac3b9669e6a
Needa Ansari <needa.0710@gmail.com>
2025-08-04 17:08:13 +0530

    login redirecct url change

--- tradingplatform/settings.py
+++ tradingplatform/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'tradingplatform.urls'
 
 LOGIN_URL = 'login/'  # default, matches Django auth view
-LOGIN_REDIRECT_URL = 'user/trade-history/'
+LOGIN_REDIRECT_URL = 'trade-history/'
 
 TEMPLATES = [
     {
